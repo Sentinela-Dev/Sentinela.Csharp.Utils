848b5a1f4e63319e6c2ae7e6910d2d6382675374
lu-css <dossantossilvalucas963@gmail.com>
2024-01-03 21:51:03 -0300

    [feat] change namespace

---
+++ Sentinela.Utils/src/Validations/Extensions/ValidationChecker.cs
@@ -0,0 +1,24 @@
+using FluentValidation;
+using Sentinela.Domain.Exceptions;
+
+namespace Sentinela.Utils.Validations.Extensions;
+
+public static class ValidatorExtension
+{
+    public static void Check<T>(this IValidator<T> validator, T value, string message)
+    {
+        var result = validator.Validate(value);
+
+        if (result.IsValid)
+        {
+            return;
+        }
+
+        var errors = result
+          .Errors
+          .Select(e => new ValidationError(e.PropertyName, e.ErrorMessage))
+          .ToList();
+
+        throw new ValidationFailException(message, errors);
+    }
+}
